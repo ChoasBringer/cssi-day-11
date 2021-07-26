5f2b24949b420c44a2e902e20f1d7fa79e92d61d
Teyon <teyonhherring72@gmail.com>
2021-07-26 19:03:42 +0000

    Multiple Sign In Option Attempt

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,7 +1,14 @@
-const signInButton = document.querySelector(".button")
+const signInButton = document.querySelector("#lol")
 signInButton.addEventListener("click", () => {
     console.log("clicked!")
-    const provider = new firebase.auth.GoogleAuthProvider()
+    const message = document.querySelector("#lol")
+    const del = document.querySelector(".hidden")
+    message.innerHTML = "Would you like to sign in using your Google Account?"
+    del.classList.remove("hidden")
+    const yes = document.querySelector('#yes')
+    const no = document.querySelector('#no')
+    yes.addEventListener("click", () => {
+        const provider = new firebase.auth.GoogleAuthProvider()
     
     firebase.auth().signInWithPopup(provider).then(result => {
         const user = result.user
@@ -12,5 +19,21 @@ signInButton.addEventListener("click", () => {
     .catcg(error => {
         console.log("login failed", error)
     })
+
+    })
+    
+    /*
+    
+    const provider = new firebase.auth.GoogleAuthProvider()
+    
+    firebase.auth().signInWithPopup(provider).then(result => {
+        const user = result.user
+
+        console.log("login success", user)
+        window.location = "writeNote.html"
+    })
+    .catcg(error => {
+        console.log("login failed", error)
+    })*/
 
 })
